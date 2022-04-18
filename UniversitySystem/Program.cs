f4f69bc4722f37c82cf50b64a83b51d471ff4b99
Emil <azsumemi@gmail.com>
2022-04-18 15:50:47 +0300

    Add zoomIn and zoomOut on university images

--- UniversitySystem/Program.cs
+++ UniversitySystem/Program.cs
@@ -16,10 +16,11 @@ namespace UniversitySystem
         public static void Main(string[] args)
         {
             var db = new ApplicationDbContext();
-            db.Database.EnsureDeleted();
-            db.Database.EnsureCreated();
-            var uniCrawler = new UniversityCrawler(db);
-            uniCrawler.Crawl().GetAwaiter().GetResult();
+            //Create db and add data
+            //db.Database.EnsureDeleted();
+            //db.Database.EnsureCreated();
+            //var uniCrawler = new UniversityCrawler(db);
+            //uniCrawler.Crawl().GetAwaiter().GetResult();
             CreateHostBuilder(args).Build().Run();
         }
 
